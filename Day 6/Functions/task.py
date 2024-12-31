9a0e208b183d0aa27a4ca94df5ef73971049caed
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-12-30 23:04:51 -0800

    day6 challenege python

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,16 @@
+print('Hello')
+name_length = len('Hello')
+print(name_length)
+
+def my_function():
+    print('Hello')
+    print('Bye')
+
+my_function()
+
+def my_func_name():
+    name = input('What is your name?: ')
+    print('Hello, ' + name)
+print('Welcome to my world!')
+
+my_func_name()
