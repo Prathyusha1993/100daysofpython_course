d673d144b0a0eca9b5b7ee09dd79bb1b5f56ff78
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-01-03 11:29:16 -0800

    day 8 challenege python

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,13 @@
+def greet():
+    print('Hello')
+    print('How are you doing?')
+    print('Welcome to the band')
+
+greet()
+
+def greet(name):
+    print(f'Hello {name}')
+    print('How are you doing?')
+    print('Welcome to the band')
+
+greet('Pooja')
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,24 @@
 # Functions with input
-
 def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
 
 
 greet_with_name("Jack Bauer")
+
+# function with multiple inputs positional arguments:
+def greet_with_multiple(name, location):
+    print(f'Hello {name}')
+    print(f'what is famous in your {location}')
+
+greet_with_multiple('Pinky', 'USA')
+greet_with_multiple('USA', 'Pinky')
+
+# keyword arguments:
+greet_with_multiple(location = 'USA', name ='Pinky')
+
+# example for keyword arguments:
+def my_func(a, b, c):
+    print(a,b,c)
+my_func(1,2,3)
+my_func(c=3, a=1,b=2)
