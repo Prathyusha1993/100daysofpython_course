0988e01c9a6fce663e5e835c0761b706219809f6
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-01-03 16:39:11 -0800

    day 10 challenege python

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    '''take a first and last name and format it to return the title case version of the name.'''
+
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
@@ -9,4 +11,18 @@ formatted_name = format_name("AnGeLa", "YU")
 length = len(formatted_name)
 
 
+# ghhjn
+# kjiygbk
+# yffxchb
+# bkhgh
 
+def my_function(a):
+    if a < 40:
+        return
+        print('Terrible')
+    if a < 80:
+        return 'pass'
+    else:
+        return 'great'
+
+print(my_function(25))
